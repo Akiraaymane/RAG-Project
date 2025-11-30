41058b27dd205f831facf2de32e95331f8de3b25
MENDYVINCE <Github02nov123@gmail.com>
2025-11-30 23:11:37 +0100

    Update retreiver.py
    Signed-off-by: MENDYVINCE <Github02nov123@gmail.com>

--- src_pipeline/retreiver.py
+++ src_pipeline/retreiver.py
@@ -1,42 +1,6 @@
-class Retriever:
-    def __init__(self, vector_store, embedder):
-        self.vector_store = vector_store
-        self.embedder = embedder
+class RetrieverBuilder:
+    def __init__(self, vectorstore):
+        self.vectorstore = vectorstore
 
-    def retrieve(self, query, n_results=5):
-        query_vector = self.embedder.model.encode(query)
-        results = self.vector_store.query(query_vector, n_results=n_results)
-
-        retrieved_chunks = []
-        for i, text in enumerate(results["documents"][0]):
-            chunk = {
-                "text": text,
-                "metadata": results["metadatas"][0][i],
-                "score": results["distances"][0][i]
-            }
-            retrieved_chunks.append(chunk)
-
-        return retrieved_chunks
-
-
-
-
-
-
-
-
-# Class Retriever:
-#     When you create a Retriever:
-#         Remember which VectorStore to use
-#         Remember which Embedder to use
-
-#     To retrieve relevant chunks for a query:
-#         Turn the query text into numbers using the Embedder
-#         Ask the VectorStore to find the top matching chunks for these numbers
-#         For each matching chunk returned:
-#             Make a dictionary with:
-#                 text -> the chunk text
-#                 metadata -> the information about the chunk (like filename)
-#                 score -> how close it is to the query
-#             Add this dictionary to the list of retrieved chunks
-#         Return the list of retrieved chunks
+    def build(self, top_k: int):
+        return self.vectorstore.as_retriever(search_kwargs={"k": top_k})
