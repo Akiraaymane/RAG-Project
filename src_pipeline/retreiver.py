ae92ebb4f1032fbb2c8abf1ee9a497e3809c6b81
MENDYVINCE <Github02nov123@gmail.com>
2025-11-30 17:10:35 +0100

    Update retreiver.py
    Signed-off-by: MENDYVINCE <Github02nov123@gmail.com>

--- src_pipeline/retreiver.py
+++ src_pipeline/retreiver.py
@@ -1,1 +1,7 @@
 
+class RetrieverBuilder:
+    def __init__(self, vectorstore):
+        self.vectorstore = vectorstore
+
+    def build(self, top_k: int):
+        return self.vectorstore.as_retriever(search_kwargs={"k": top_k})
