32efa793715f387e63f997ab3ad384c86e271887
MENDYVINCE <Github02nov123@gmail.com>
2025-11-30 17:42:11 +0100

    Update retreiver.py
    Empty to add group work
    
    Signed-off-by: MENDYVINCE <Github02nov123@gmail.com>

--- src_pipeline/retreiver.py
+++ src_pipeline/retreiver.py
@@ -1,7 +1,1 @@
 
-class RetrieverBuilder:
-    def __init__(self, vectorstore):
-        self.vectorstore = vectorstore
-
-    def build(self, top_k: int):
-        return self.vectorstore.as_retriever(search_kwargs={"k": top_k})
