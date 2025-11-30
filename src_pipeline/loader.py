7ca1d3e101ede0cda2b62d2007b66275f6e1448e
MENDYVINCE <Github02nov123@gmail.com>
2025-11-30 23:20:52 +0100

    Update loader.py
    Signed-off-by: MENDYVINCE <Github02nov123@gmail.com>

--- src_pipeline/loader.py
+++ src_pipeline/loader.py
@@ -1,1 +1,32 @@
 
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+
+class DocumentLoader:
+    def __init__(self, data_dir: str):
+        self.data_dir = data_dir
+
+    def load(self):
+        loader = DirectoryLoader(
+            self.data_dir,
+            glob="*.md",
+            loader_cls=TextLoader,
+            show_progress=True
+        )
+        docs = loader.load()
+
+        splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=[
+                ("#", "title"),
+                ("##", "section"),
+                ("###", "subsection"),
+            ]
+        )
+
+        chunks = []
+        for doc in docs:
+            parts = splitter.split_text(doc.page_content)
+            for p in parts:
+                p.metadata = doc.metadata
+                chunks.append(p)
+        return chunks
