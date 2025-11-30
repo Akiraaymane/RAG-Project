adc8f4077ebb76f6efecc8ca7a48e3b11a54ad17
MENDYVINCE <Github02nov123@gmail.com>
2025-11-30 17:40:52 +0100

    Update llm.py
    Empty to add group Work
    
    Signed-off-by: MENDYVINCE <Github02nov123@gmail.com>

--- src_pipeline/llm.py
+++ src_pipeline/llm.py
@@ -1,13 +1,1 @@
-from langchain_groq import ChatGroq
 
-class LLMBuilder:
-    def __init__(self, api_key: str, model_name: str):
-        self.api_key = api_key
-        self.model_name = model_name
-
-    def build(self):
-        return ChatGroq(
-            api_key=self.api_key,
-            model=self.model_name,
-            temperature=0
-        )
